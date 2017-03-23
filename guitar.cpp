bc5e1be5175f3165b16f8687aea2536f213567c5
Eugene Zverev <zhenya1998@mail.ru>
2017-03-23 22:03:24 +0300

    Second commit

--- guitar.cpp
+++ guitar.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 class Guitar{
 	private:
-		float fretboard;
-		bool amplifier;	
-		int quantity_s;
-		float thickness_s;
-		string material_s;
-		string mfr_guitar;
+		float fretboard; //длина мензуры в дюймах
+		bool amplifier;	//положение усилителя (1 или 0)
+		int quantity_s; //количество струн
+		float thickness_s; //толщина 1 струны в дюймах
+		string material_s; //материал струн
+		string mfr_guitar; //изготовитель гитары
+		string type_guitar; //тип гитары
 	public:
-		bool getAmplifier(){ //возвращает положение усилителя (1 или 2)
+		bool getAmplifier(){ //возвращает положение усилителя (0 или 1)
 			return amplifier;
 		}
 		float getThickness(){ //возвращает толщину 1 струны
@@ -36,10 +37,42 @@ class Guitar{
 			}
 			if(this->amplifier==true) cout<<"Amplifier is connected"; else cout<<"Amplifier isn't connected"; //если усилитель подключён ..., иначе ...
 		}
+		Guitar(){ //конструктор по умолчанию
+			material_s = "steel";
+			thickness_s = 0.010;
+			quantity_s = 6;
+			amplifier = false;
+			fretboard = 25.5;
+			string mfr_guitar = "unknown";
+			string type_guitar = "acoustic";		
+		}
+		Guitar(string type_guitar, float fretboard, string mfr_guitar, bool amplifier, int quantity_s, float thickness_s, string material_s){ //конструктор с параметрами
+			this->fretboard = fretboard;
+			this->amplifier = amplifier;
+			this->quantity_s = quantity_s;
+			this->thickness_s = thickness_s;
+			this->material_s = material_s;
+		}
+		Guitar(const Guitar &object){ //конструктор копирования
+			material_s = object.material_s;
+			thickness_s = object.thickness_s;
+			quantity_s = object.quantity_s;
+			amplifier = object.amplifier;
+			fretboard = object.fretboard;	
+			string mfr_guitar = object.mfr_guitar; 
+			string type_guitar = object.type_guitar;	
+		}
+		
+		
+		
 };
 
-int main(){
 
+int main(){
+	Guitar acoustic_1;
+	Guitar les_paul("electric", 24.75, "gibson", false, 6, 0.010, "steel");
+	Guitar explorer(les_paul);
+	
 	getch();
 	return 0;
 }
