685522367f5eec8dd393dc617a06e66644d8dc4f
Eugene Zverev <zhenya1998@mail.ru>
2017-03-24 15:42:34 +0300

    Fifth commit

--- guitar.cpp
+++ guitar.cpp
@@ -23,6 +23,16 @@ class Guitar{
 		string getMaterial(){ //возвращает материал струн
 			return material_s;
 		}
+		void show_description(){
+			cout<<"   Mensura: "<<fretboard<<" inches;"<<endl;	
+			cout<<"   Amplifier status: ";
+			if(amplifier==true) cout<<"amplifier is connected;"<<endl; else cout<<"amplifier isn't connected;"<<endl; //если усилитель подключён ..., иначе ...
+			cout<<"   Number of strings: "<<quantity_s<<";"<<endl;	
+			cout<<"   Thickness of the first string: "<<thickness_s<<" inches;"<<endl;	
+			cout<<"   String material: "<<material_s<<";"<<endl;	
+			cout<<"   Guitar manufacturer: "<<mfr_guitar<<";"<<endl;	
+			cout<<"   Guitar type: "<<type_guitar<<"."<<endl<<endl;	
+		}
 		void plug_amplifier(bool amplifier){ //подключить усилитель
 			if(amplifier==false){ //если усилитель подключён ...
 				cout<<"Do you want to connect an amplifier? (yes or no)"<<endl;
@@ -43,8 +53,8 @@ class Guitar{
 			quantity_s = 6;
 			amplifier = false;
 			fretboard = 25.5;
-			string mfr_guitar = "unknown";
-			string type_guitar = "acoustic";		
+			mfr_guitar = "unknown";
+			type_guitar = "acoustic";		
 		}
 		Guitar(string type_guitar, float fretboard, string mfr_guitar, bool amplifier, int quantity_s, float thickness_s, string material_s){ //конструктор с параметрами
 			this->fretboard = fretboard;
@@ -52,6 +62,8 @@ class Guitar{
 			this->quantity_s = quantity_s;
 			this->thickness_s = thickness_s;
 			this->material_s = material_s;
+			this->type_guitar = type_guitar;
+			this->mfr_guitar = mfr_guitar;
 		}
 		Guitar(const Guitar &object){ //конструктор копирования
 			material_s = object.material_s;
@@ -59,12 +71,13 @@ class Guitar{
 			quantity_s = object.quantity_s;
 			amplifier = object.amplifier;
 			fretboard = object.fretboard;	
-			string mfr_guitar = object.mfr_guitar; 
-			string type_guitar = object.type_guitar;	
+			mfr_guitar = object.mfr_guitar; 
+			type_guitar = object.type_guitar;	
 		}
-		
-		
+			
 };
+
+
 
 
 int main(){
@@ -77,6 +90,23 @@ int main(){
 	cout<<"   Case: mahogany;"<<endl<<"   Neck: mahogany, 22 fret;"<<endl<<"   Fretboard: rosewood;"<<endl<< "   Mensura: 24.75 inches;"<<endl<<"   Fixing the neck: pasted."<<endl;
 	cout<<endl<<"3. Acoustic guitar - six-string acoustic guitar, classical body."<<endl;
 	cout<<"   Case: mahogany;"<<endl<<"   Neck: mahogany, 20 fret;"<<endl<<"   Fretboard: rosewood Sonokeling;"<<endl<< "   Mensura: 25.5 inches;"<<endl<<"   Lower deck and shells: from Indonesian mahogany."<<endl;
+	cout<<endl<<"Enter the guitar number (1,2 or 3): ";
+	int num;
+	while ((!(cin >> num) || (cin.peek() != '\n')) || (num!=1 && num!=2 && num!=3)){ //пока вводится не (число без лишних символов) или это число <> 1, 2 или 3 ...
+		cout<<"Error!!! Enter the correct number (1, 2 or 3): ";
+		cin.clear(); 
+		cin.sync(); 
+	}	
+	Guitar current;
+	if(num==1) current = les_paul; //если номер гитары 1, 2 или 3 ...
+		else if(num==2) current = explorer; 
+			else current = acoustic_1;
+	current.show_description();
+	cout<<"What do you want to do?"<<endl;
+	cout<<"   1. Connect an amplifier;"<<endl;
+	cout<<"   2. Pull the strings."<<endl<<endl;
+	//cout<<"Type your answer: ";
+	
 	
 	
 	
